c8177ee65ee32490194d0f8f427f940d6a5e4b87
Carlos Gomez <cfgomez@us.ibm.com>
2025-08-11 11:44:27 -0500

    updated to new provider version

--- Enterprise-SRE-Team/terraform/SRE-Team.tf
+++ Enterprise-SRE-Team/terraform/SRE-Team.tf
@@ -2,13 +2,31 @@ terraform {
   required_providers {
     ibm = {
       source = "IBM-Cloud/ibm"
-      version = "1.58.0"
+      version = "1.77.1"
     }
   }
 }
 
+variable "apikey" {
+ type = string
+ description = "Apikey with access to Create and assign IAM templates"
+ nullable = false
+}
+
+variable "assignment_target_type" {
+  type = string
+  description = "Assigning to a single Account or an AccountGroup"
+  nullable = false
+}
+
+variable "assignment_target" {
+  type = string
+  description = "Account Id or Account Group Id where to assign the template"
+  nullable = false
+}
+
 provider "ibm" {
-  ibmcloud_api_key = "<your apikey>"
+  ibmcloud_api_key = var.apikey
 }
 
 # Create Kubernetes administrator policy template
@@ -54,22 +72,24 @@ resource "ibm_iam_trusted_profile_template" "SRE_Team_tp_template" {
   profile {
     name = "Enterprise SRE Team"
     rules {
-      type = "<rule type>"
-      realm_name = "<realm name>"
+      type = "Profile-SAML"
+      realm_name = "https://myIdp.mycompany.com"
       expiration = 3600
       conditions {
-        claim = "<claim field name>"
+        claim = "Team"
         operator = "EQUALS"
-        value = "\"<value to compare>\""
+        value = "\"SRE\""
       }
     }
   }
   policy_template_references {
-	id = split("/", ibm_iam_policy_template.Kube_Administrator_ap_template.id)[0]
+	# id = split("/", ibm_iam_policy_template.Kube_Administrator_ap_template.id)[0]
+	id = ibm_iam_policy_template.Kube_Administrator_ap_template.template_id
 	version = ibm_iam_policy_template.Kube_Administrator_ap_template.version
   }
   policy_template_references {
-	id = split("/", ibm_iam_policy_template.MongoDb_Administrator_ap_template.id)[0]
+	# id = split("/", ibm_iam_policy_template.MongoDb_Administrator_ap_template.id)[0]
+	id = ibm_iam_policy_template.MongoDb_Administrator_ap_template.template_id
 	version = ibm_iam_policy_template.MongoDb_Administrator_ap_template.version
   }
   committed = true
@@ -79,8 +99,8 @@ resource "ibm_iam_trusted_profile_template" "SRE_Team_tp_template" {
 resource "ibm_iam_trusted_profile_template_assignment" "tp_assignment_instance" {
   template_id = split("/", ibm_iam_trusted_profile_template.SRE_Team_tp_template.id)[0]
   template_version = ibm_iam_trusted_profile_template.SRE_Team_tp_template.version
-  target_type = "AccountGroup" // or "Account"
-  target = "<account group id>" // or "<account id>"
+  target_type = var.assignment_target_type
+  target = var.assignment_target
   depends_on = [
     ibm_iam_trusted_profile_template.SRE_Team_tp_template
   ]
